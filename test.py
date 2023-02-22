9658bd8e8cca050c463cd4e1be97326475a78dbd
Siddharth Dyamgond <81026894+Siddharth2391@users.noreply.github.com>
2023-02-22 19:37:19 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -3,20 +3,38 @@ from PIL import ImageTk,Image
 import random
 import tkinter.messagebox as tmsg
 
+app=Tk()
+count=0
+
+def generate():
+    global comp
+    comp=random.randint(1, 101)
+
 def basic():
+    #setup the window size, title, logo
     app.title("Number Guessing game")
     app.geometry("500x500")
     app.minsize(500,500)
     app.maxsize(500,500)
     photo = PhotoImage(file = "guess.png")
     app.iconphoto(False, photo)
-
     heading=Label(text='Number Guessing game',font="Helvicta 18 bold",bg='black',fg='tomato',padx=170).pack()
     with open('score.txt','r') as f:
         hg=f.read()
-    
     sc=Label(app,text=f'Previous score: {hg}',font='lucida 8 bold ').pack(anchor=E,padx=25,pady=5)
+    
+    #footer
     footer=Label(text='Developed by Siddharth Dyamgond',font="Helvicta 8 bold",bg='black',fg='tomato',padx=153).pack(side=BOTTOM)
+
+    #Setup Menu
+    mymenu=Menu(app)
+    filee=Menu(mymenu,tearoff=0)
+    mymenu.add_cascade(label='Start',menu=filee)
+    mymenu.add_cascade(label='Restart',command=restart)
+    mymenu.add_command(label='About',command=call1)
+    mymenu.add_command(label='Quit',command=quit)
+    app.config(menu=mymenu)
+    generate()
 
 def result():
     global count
@@ -34,30 +52,23 @@ def result():
             show.config(text='Winn!',fg='green')
             with open('score.txt','w') as f:
                 f.write(str(score))
+            generate()
+            tmsg.showinfo('Next number',f'click ok to Guess another number')
         elif comp>n:
             show.config(text='Select greater number',fg='red')
         else:
             show.config(text='Select smaller number',fg='red')
 
 def restart():
-    pass
+    tmsg.showerror('Reset',"Game reset!")
+    generate()
 
 def call1():
     str1='This game is developed by XD\n\ncopyright@2021-22 '
     tmsg.showinfo('About',str1)
-    
-app = Tk()
-basic()
-count=0
-mymenu=Menu(app)
-filee=Menu(mymenu,tearoff=0)
-mymenu.add_cascade(label='Start',menu=filee)
-mymenu.add_cascade(label='Restart',command=restart)
-mymenu.add_command(label='About',command=call1)
-mymenu.add_command(label='Quit',command=quit)
-app.config(menu=mymenu)
 
-comp=random.randint(1, 101)
+basic()
+
 print(comp)
 userv=StringVar()
 user=Entry(app,textvariable=userv,justify=CENTER,relief=FLAT,borderwidth=2,font='Helvicta 18 bold').pack(pady=10) 
